74b2ee5d1a95c2ac7e328783ad616ed443ac633b
salma <salmafassi72@gmail.com>
2022-05-16 12:38:32 +0000

    my commit

---
+++ projet1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using projet1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetProprietaire()
+        {
+            ModelBD db = new ModelBD();
+            //DateTime mydate = DateTime.ParseExact(DateTime.Now.ToString(), "dd/MM/yyyy"+ " 00:00:00", System.Globalization.CultureInfo.InvariantCulture);
+            //Console.WriteLine(mydate);
+            var prpActue = db.proprietaires.OrderBy(x => x.id).Where(x=>x.dateAjout.Day.Equals(DateTime.Now.Day) && x.dateAjout.Month.Equals(DateTime.Now.Month) && x.dateAjout.Year.Equals(DateTime.Now.Year)).ToList();
+            return Json(new { data = prpActue }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetClient()
+        {
+            ModelBD db = new ModelBD();
+        
+        var prpActue = db.clients.OrderBy(x => x.id).Where(x => x.dateAjout.Day.Equals(DateTime.Now.Day) && x.dateAjout.Month.Equals(DateTime.Now.Month) && x.dateAjout.Year.Equals(DateTime.Now.Year)).ToList();
+            return Json(new { data = prpActue}, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+           
+           
+            return View();
+        }
+    }
+}
---
+++ projet1/Controllers/MesProduitController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static projet1.ModelBD;
+
+namespace projet1.Controllers
+{
+    public class MesProduitController : Controller
+    {
+        // GET: MesProduit
+        public ActionResult Mesproduits()
+        {
+            ModelBD db = new ModelBD();
+            var product = db.produits.OrderBy(a => a.ref_Id).ToList();
+            return View(product);
+        }
+        [HttpGet]
+        public ActionResult NouveauProduit()
+        {
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NouveauProduit(produit p, int CategorieId)
+        {
+            string filename = Path.GetFileNameWithoutExtension(p.ImageFile.FileName);
+            string extension = Path.GetExtension(p.ImageFile.FileName);
+            filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+            p.ImagePath = "~/Image/" + filename;
+            filename = Path.Combine(Server.MapPath("~/Image/"), filename);
+            p.ImageFile.SaveAs(filename);
+
+            produit p1 = new produit();
+            p1.title = p.title;
+            p1.description = p.description;
+            p1.prix = p.prix;
+            p1.utilisateurId = 28;
+            p1.dateAjout = DateTime.Now;
+            p1.CategorieId = CategorieId;
+            p1.statusP = produit.StatusProduit.NonVendu;
+            p1.ImagePath = p.ImagePath;
+            p1.ImageFile = p.ImageFile;
+            using (var modbdel = new ModelBD())
+            {
+                if (ModelState.IsValid)
+                {
+                    if (Request["sub"] == "ajouter")
+                    {
+                        modbdel.produits.Add(p1);
+                        modbdel.SaveChanges();
+
+                        return RedirectToAction("MesProduits");
+                    }
+                    else if (Request["sub"] == "annuler")
+                    {
+                        return RedirectToAction("NouveuProduit");
+                    }
+                    else
+                    {
+                        return View("NouveauProduit");
+                    }
+
+                }
+                ModelState.Clear();
+                return View(p);
+            }
+
+        }
+    }
+}
